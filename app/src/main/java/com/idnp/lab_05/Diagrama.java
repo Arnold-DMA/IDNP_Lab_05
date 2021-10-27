f675a1148428ce1901aba41ebc97fd0ace963826
Arnold <amolloa@unsa.edu.pe>
2021-10-26 19:28:26 -0500

    Interfaz básica de gráfico

--- app/src/main/java/com/idnp/lab_05/Diagrama.java
+++ app/src/main/java/com/idnp/lab_05/Diagrama.java
@@ -4,15 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 
 public class Diagrama extends AppCompatActivity {
 
+    String [] nombres;
+    String [] valores;
+    double [] valoresDouble;
+    String color;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_diagrama);
+        setContentView(new Barras(this));
+
+        Bundle datos = getIntent().getExtras();
+        nombres = datos.getStringArray("Nombres");
+        valores = datos.getStringArray("Valores");
+        valoresDouble = new double[]{Integer.parseInt(valores[0]),
+                Integer.parseInt(valores[1]),
+                Integer.parseInt(valores[2]),
+                Integer.parseInt(valores[3])};
+        color = datos.getString("Color");
     }
 
     public class Barras extends View {
@@ -22,7 +38,24 @@ public class Diagrama extends AppCompatActivity {
         }
 
         protected void onDraw(Canvas canvas){
-
+            Paint pincel = new Paint();
+            pincel.setColor(Color.BLACK);
+            pincel.setStrokeWidth(2);
+            pincel.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(50, 100, 1000, 800, pincel);
+            canvas.drawLine(150, 700, 950, 700, pincel);
+            pincel.setStrokeWidth(3);
+            pincel.setTextSize(40);
+            pincel.setTextAlign(Paint.Align.CENTER);
+            canvas.drawText("Nombre populares de mascotas",475, 150, pincel);
+            pincel.setTextSize(30);
+            pincel.setTextAlign(Paint.Align.RIGHT);
+            canvas.drawText("0",120, 710, pincel);
+            canvas.drawText("20",120, 610, pincel);
+            canvas.drawText("40",120, 510, pincel);
+            canvas.drawText("60",120, 410, pincel);
+            canvas.drawText("80",120, 310, pincel);
+            canvas.drawText("100",120, 210, pincel);
         }
 
     }
--- app/src/main/java/com/idnp/lab_05/MainActivity.java
+++ app/src/main/java/com/idnp/lab_05/MainActivity.java
@@ -3,15 +3,22 @@ package com.idnp.lab_05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner colores;
+    EditText et1, et2, et3, et4, en1, en2, en3, en4;
+    Button btn1;
+    String [] nombres;
+    String [] valores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +26,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         colores = (Spinner) findViewById(R.id.ListColor);
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+        et3 = (EditText) findViewById(R.id.et3);
+        et4 = (EditText) findViewById(R.id.et4);
+        en1 = (EditText) findViewById(R.id.en1);
+        en2 = (EditText) findViewById(R.id.en2);
+        en3 = (EditText) findViewById(R.id.en3);
+        en4 = (EditText) findViewById(R.id.en4);
+        btn1 = (Button) findViewById(R.id.btn1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ListaColores, android.R.layout.simple_spinner_item);
         colores.setAdapter(adapter);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle datos = new Bundle();
+                nombres = new String[]{et1.getText().toString(),
+                                        et2.getText().toString(),
+                                        et3.getText().toString(),
+                                        et4.getText().toString()};
+
+                valores = new String[]{en1.getText().toString(),
+                                        en2.getText().toString(),
+                                        en3.getText().toString(),
+                                        en4.getText().toString()};
+
+                datos.putStringArray("Nombres", nombres);
+                datos.putStringArray("Valores", valores);
+                datos.putString("Color", colores.getSelectedItem().toString());
+
+                Intent intent = new Intent(getApplicationContext(), Diagrama.class);
+                intent.putExtras(datos);
+                startActivity(intent);
+            }
+        });
     }
 
 }
